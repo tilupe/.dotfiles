3ae6ac7bc2aafe452f0943f7cb5a432e93448dd5
Timothy Pescatore <timothy.pescatore@digitecgalaxus.ch>
2023-03-31 15:50:28 +0200

    use nvim tree instead of neotree

--- neovim/.config/nvim/lua/plugins/neotree.lua
+++ neovim/.config/nvim/lua/plugins/neotree.lua
@@ -1,24 +1,50 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    cmd = "Neotree",
-    keys = {
-      {
-        "<leader>fe",
-        function()
-          require("neo-tree.command").execute({ toggle = true, dir = require("util").get_root() })
-        end,
-        desc = "Explorer NeoTree (root dir)",
+  -- {
+  --   "nvim-neo-tree/neo-tree.nvim",
+  --   cmd = "Neotree",
+  --   keys = {
+  --     {
+  --       "<leader>fe",
+  --       function()
+  --         require("neo-tree.command").execute({ toggle = true, dir = "%" })
+  --       end,
+  --       desc = "Explorer NeoTree (root dir)",
+  --     },
+  --     {
+  --       "<leader>fE",
+  --       function()
+  --         require("neo-tree.command").execute({ toggle = true, dir = require("util").get_root() })
+  --       end,
+  --       desc = "Explorer NeoTree (root)",
+  --     },
+  --   },
+  -- },
+
+  "nvim-tree/nvim-tree.lua",
+  cmd = { "NvimTreeToggle" },
+  keys = {
+    { "<leader>fe", "<cmd>NvimTreeToggle<cr>", desc = "Explorer" },
+  },
+  opts = {
+    disable_netrw = false,
+    hijack_netrw = true,
+    respect_buf_cwd = true,
+    view = {
+      number = true,
+      relativenumber = true,
+    },
+    filters = {
+      custom = { ".git" },
+    },
+    sync_root_with_cwd = true,
+    update_focused_file = {
+      enable = true,
+      update_root = true,
+    },
+    actions = {
+      open_file = {
+        quit_on_open = true,
       },
-      {
-        "<leader>fE",
-        function()
-          require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
-        end,
-        desc = "Explorer NeoTree (cwd)",
-      },
-      { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
-      { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
     },
   },
 }
