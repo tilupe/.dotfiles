6bcb4150110ae456c86ee295677168b9762c17fb
Timothy Pescatore <timothy.pescatore@digitecgalaxus.ch>
2023-04-09 15:02:39 +0200

    use correct keymap

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -28,7 +28,7 @@ return {
     { "<leader>sd", "<cmd>Telescope diagnostics<cr>", desc = "Diagnostics" },
     { "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
     { "<leader>sH", "<cmd>Telescope highlights<cr>", desc = "Search Highlight Groups" },
-    { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
+    { "<leader>sm", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
     { "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
     { "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
     { "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
