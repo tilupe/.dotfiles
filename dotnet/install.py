256941d5a90b1e82008283aec1b9d62465fa0cf7
Timothy Pescatore <timothy.pescatore@digitecgalaxus.ch>
2023-06-05 21:42:32 +0200

    update

--- dotnet/install.py
+++ dotnet/install.py
@@ -3,27 +3,55 @@ from typing import Optional
 import requests
 import json
 from bs4 import BeautifulSoup as bs
+from packaging import version
 import subprocess
 import getpass
+import shutil
+import urllib.request
 
 
-def get_latest_dotnet_version() -> Optional[str]:
+def is_dotnet_installation_necessary() -> bool:
+    if shutil.which("dotnet") is None:
+        return True
+    latest_release_version = get_latest_dotnet("latest-release")
+    output = subprocess.check_output(["dotnet", "--list-sdks"], universal_newlines=True)
+    lines = output.strip().split("\n")
+    latest_installed_version = lines[0].split()[0]
+    if version.parse(latest_installed_version) >= version.parse(latest_release_version):
+        print(
+            "dotnet skd version is already up to date with version {}".format(
+                latest_installed_version
+            )
+        )
+        return False
+    return True
+
+
+def get_latest_dotnet(field_name: str) -> str:
     dotnet_release_index_path = "https://raw.githubusercontent.com/dotnet/core/main/release-notes/releases-index.json"
-    response = requests.get(dotnet_release_index_path)
+    try:
+        response = requests.get(dotnet_release_index_path)
+    except Exception as error:
+        raise error
     if response.status_code != 200:
-        print("Failed to download from " + dotnet_release_index_path)
-        return None
+        raise Exception(
+            "Failed to download from "
+            + dotnet_release_index_path
+            + " \n status code was: "
+            + response.status_code
+        )
+
     content = response.text
     release_index = json.loads(content)
+
     for release in release_index["releases-index"]:
         if release["support-phase"] == "active":
-            return release["channel-version"]
-    print("content was wrong:\n" + content)
-    return None
+            return release[field_name]
+    raise Exception("No release found in json: " + response.text)
 
 
 def get_latest_dotnet_sdk_download_link() -> Optional[str]:
-    latest_version = get_latest_dotnet_version()
+    latest_version = get_latest_dotnet("channel-version")
     if latest_version is None:
         return None
     baseurl = "https://dotnet.microsoft.com"
@@ -38,20 +66,32 @@ def get_latest_dotnet_sdk_download_link() -> Optional[str]:
     linux_tr = linux_th.parent
     a_tag = linux_tr.find("a", string="x64")
     href_content = a_tag["href"]
-    return baseurl + href_content
-
-
-def dowload_latest_dotnet_sdk() -> str:
-    url = get_latest_dotnet_sdk_download_link()
-    save_path = "/tmp"
+    url = baseurl + href_content
     response = requests.get(url)
-    response.raise_for_status()
+    if response.status_code != 200:
+        print("Failed to download from " + downloads_url)
+        return None
+    soup = bs(response.content, "html.parser")
+    div = soup.find("div", class_="swim-container")
+    direct_link_div = div.find("div", class_="direct-link")
+    a_tag_two = direct_link_div.find("a", id="directLink")
+    href_content = a_tag_two["href"]
+    return href_content
+
+
+def dowload_latest_dotnet_sdk(save_path: str) -> str:
+    url = get_latest_dotnet_sdk_download_link()
     file_name = os.path.basename(url)
-    save_file_path = os.path.join(save_path, file_name)
-    with open(save_file_path, "wb") as file:
-        file.write(response.content)
+    print("Downloading from {}".format(url))
+    command = [
+        "wget",
+        "-O",
+        save_path,
+        url,
+    ]
+    subprocess.run(command)
     print("File '{}' downloaded successfully.".format(file_name))
-    return save_path + "/" + file_name
+    return save_path + file_name
 
 
 def create_dotnet_directory(sudo_password: str, install_dir: str):
@@ -74,14 +114,17 @@ def unpack_binary(sudo_password: str, install_dir: str, file_path: str):
 
 
 def create_soft_link_for_dotnet_command(sudo_password: str, install_dir: str):
+    symbolic_link = "/usr/local/bin/dotnet"
+    if os.path.exists(symbolic_link):
+        return
     # Todo check if file exists already
     command = [
         "sudo",
         "-S",
         "ln",
         "-s",
-        install_dir + "/dotnet",
-        "/usr/local/bin/dotnet",
+        install_dir,
+        symbolic_link,
     ]
     subprocess.run(command, input=sudo_password.encode("utf-8"))
 
@@ -92,12 +135,14 @@ def delete_file(sudo_password: str, file_path: str):
 
 
 def install_latest_dotnet_sdk():
-    file_path = dowload_latest_dotnet_sdk()
+    # if not is_dotnet_installation_necessary():
+    # return
+    file_path = dowload_latest_dotnet_sdk("~/tmp")
     sudo_password = getpass.getpass("Enter your sudo password: ")
     install_dir = "/opt/dotnet"
 
     create_dotnet_directory(sudo_password, install_dir)
-    unpack_binary(sudo_password, install_dir, file_path)
+    unpack_binary(sudo_password, file_path, install_dir)
     create_soft_link_for_dotnet_command(sudo_password, install_dir)
     delete_file(sudo_password, file_path)
 
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,9 +1,8 @@
 local wezterm = require("wezterm")
 
 return {
-	color_scheme = "GruvboxDark",
-	font_size = 13.0,
-	window_background_opacity = 0.9,
+	font_size = 14.0,
+	line_height = 0.95,
 	font = wezterm.font_with_fallback({
 		-- {
 		-- 	family = "LeagueMono",
@@ -14,8 +13,75 @@ return {
 		"JetBrains Mono NF",
 		"MesloLGS NF",
 	}),
+	automatically_reload_config = true,
+	adjust_window_size_when_changing_font_size = false,
 	hide_tab_bar_if_only_one_tab = true,
+	max_fps = 144,
+	animation_fps = 1,
+
+	enable_tab_bar = false,
+
+	allow_square_glyphs_to_overflow_width = "Never",
+	freetype_load_target = "HorizontalLcd",
+	freetype_render_target = "HorizontalLcd",
+	use_cap_height_to_scale_fallback_fonts = true,
+	warn_about_missing_glyphs = false,
+
 	window_decorations = "NONE",
+	window_background_opacity = 0.8,
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+
 	audible_bell = "Disabled",
+	visual_bell = {
+		fade_in_duration_ms = 5,
+		fade_out_duration_ms = 5,
+		target = "CursorColor",
+	},
+
+	-- default_cursor_style = "SteadyBlock",
+	cursor_blink_rate = 400,
+	default_cursor_style = "BlinkingBlock",
+	force_reverse_video_cursor = true,
 	window_close_confirmation = "NeverPrompt",
+
+	--colors = {
+	--	background = "#191b1f",
+	--	-- foreground = "#e3e5e8",
+	--	foreground = "#d3d5d8",
+	--	cursor = "#f2b90d",
+	--	cursor_fg = "#191b1f",
+	--	cursor_bg = "#ffffff",
+	--	cursor_border = "#eeeeee",
+	--	selection_bg = "#303233",
+	--	selection_fg = "#cacecd",
+	--	scrollbar_thumb = "#16161d",
+	--	split = "#16161d",
+	--	ansi = {
+	--		"#282c34",
+	--		"#c2290a",
+	--		"#66b814",
+	--		"#f2b90d",
+	--		"#06a8f9",
+	--		"#e06ef7",
+	--		"#0ac2c2",
+	--		"#d5d7dd",
+	--	},
+	--	brights = {
+	--		"#595e68",
+	--		"#f2330d",
+	--		"#80e619",
+	--		"#f5c73d",
+	--		"#38b9fa",
+	--		"#eb9efa",
+	--		"#0df2f2",
+	--		"#e3e5e8",
+	--	},
+	--},
+
+	color_scheme = "GruvboxDark",
 }
