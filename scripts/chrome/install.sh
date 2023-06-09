e2e01bedd3b62d3b8cd0af9fcf90ccd1f5ae43bb
Timothy Pescatore <timothy.pescatore@digitecgalaxus.ch>
2023-06-09 18:11:57 +0200

    cleanup

--- scripts/chrome/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-PWD=$(pwd)
-
-sudo apt install libu2f-udev
-
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
-sudo dpkg -i $PWD/google-chrome-stable_current_amd64.deb
-rm $PWD/google-chrome-stable_current_amd64.deb
-
--- scripts/nvim/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PWD=$(pwd)
-wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-sudo chmod u+x $PWD/nvim.appimage
-
-sudo mv $PWD/nvim.appimage /usr/bin/nvim
-sudo rm /usr/bin/vi
-sudo ln -s /usr/bin/nvim /usr/bin/vi
-
