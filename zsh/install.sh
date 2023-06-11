de9f875e7866f07dd7eab9ff2e07b0f137f7ed38
thinkpad <thinkpad@pop-os.localdomain>
2023-06-11 16:31:25 +0200

    update zsh install

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-pwd=$(pwd)
 sudo apt install zsh &&
 zsh --version &&
 chsh -s "$(which zsh)"
 
-
-mkdir -p ~/.zsh
-cd ~/.zsh || exit
-
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-
-sd 'plugins=\(.*\)' 'plugins=(git docker)'
-
-cd "$pwd" || exit
