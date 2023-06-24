ce4e82881503fd8af4a42768c1119610c90a9671
Timothy Pescatore <timothy.pescatore@digitecgalaxus.ch>
2023-06-24 14:08:41 +0200

    update keymaps for git

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -8,7 +8,7 @@ return {
   "MunifTanjim/nui.nvim",
   {
     "nvim-tree/nvim-web-devicons",
-    config = { default = true },
+    config = true,
   },
   -- { "nacro90/numb.nvim", event = "BufReadPre", config = true },
   -- {
@@ -36,11 +36,14 @@ return {
   {
     "tpope/vim-fugitive",
     keys = {
-      { "<leader>gs", "<cmd>Git<cr>", desc = "Status" },
-      { "<leader>gl", "<cmd>Gvdiffsplit<cr>", desc = "Git diff right" },
+      { "<leader>gg", "<cmd>Git<cr>", desc = "Status" },
       { "<leader>gb", "<cmd>Git blame<cr>", desc = "Blame" },
-      { "<leader>gP", "<cmd>Git push<cr>", desc = "Push" },
+      { "<leader>gP", "<cmd>Git -c push.default=current push<cr>", desc = "Push" },
       { "<leader>gp", "<cmd>Git pull<cr>", desc = "Pull" },
+      { "<leader>gdf", "<cmd>Gvdiffsplit<cr>", desc = "File" },
+      { "<leader>gdm", "<cmd>Gvdiff origin/master<cr>", desc = "Master" },
+      { "<leader>gsf", "<cmd>Gwrite<cr>", desc = "File" },
+      { "<leader>gsR", "<cmd>Gwrite<cr>", desc = "Reset File" },
     },
   },
   {
@@ -65,14 +68,13 @@ return {
         -- stylua: ignore start
         map("n", "<leader>gj", gs.next_hunk, "Next Hunk")
         map("n", "<leader>gk", gs.prev_hunk, "Prev Hunk")
-        map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
-        map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+        map({ "n", "v" }, "<leader>gsh", ":Gitsigns stage_hunk<CR>", "Hunk")
+        map({ "n", "v" }, "<leader>gsr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
         map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
         map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
         map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
         map("n", "<leader>ghp", gs.preview_hunk, "Preview Hunk")
         map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
-        map("n", "<leader>ghd", gs.diffthis, "Diff This")
         map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
         map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
       end,
@@ -81,7 +83,7 @@ return {
   {
     "sindrets/diffview.nvim",
     keys = {
-      { "<leader>go", "<cmd>DiffviewOpen<cr>", desc = "DiffOpen" },
+      { "<leader>gdo", "<cmd>DiffviewOpen<cr>", desc = "DiffviewOpen" },
     },
   },
   {
@@ -92,6 +94,18 @@ return {
     },
   },
 
+  -- Markdown
+  { "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
+  { "iamcco/markdown-preview.nvim", config = true, cmd = "MarkdownPreview" },
+
+  ---- Mini -----
+  {
+    "echasnovski/mini.files",
+    event = "VeryLazy",
+    config = function(_, opts)
+      require("mini.files").setup(opts)
+    end,
+  },
   -- auto pairs
   {
     "echasnovski/mini.pairs",
@@ -147,7 +161,4 @@ return {
       { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
     },
   },
-  -- Markdown
-  { "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
-  { "iamcco/markdown-preview.nvim", config = true, cmd = "MarkdownPreview" },
 }
