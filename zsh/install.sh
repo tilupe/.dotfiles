eb23260da441983460678a0afa79f3635371cf8c
Timothy Pescatore <timothy.pescatore@digitecgalaxus.ch>
2023-07-09 22:00:35 +0200

    update zsh

--- zsh/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo apt install zsh &&
-	zsh --version &&
-	chsh -s "$(which zsh)"
-
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh && zplug install
