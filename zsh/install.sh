23e6d23f4a372105e13930b827f0c4cd216a2934
Timothy Pescatore <timothy.pescatore@digitecgalaxus.ch>
2023-07-09 22:00:59 +0200

    add zshrc to .config/zsh

---
+++ zsh/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt install zsh &&
+	zsh --version &&
+	chsh -s "$(which zsh)"
+
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh && zplug install
