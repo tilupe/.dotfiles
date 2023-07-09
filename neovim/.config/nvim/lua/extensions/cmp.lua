98a33eb1b878517e3da6fdd5e8b345d038b4f88e
Timothy Pescatore <timothy.pescatore@digitecgalaxus.ch>
2023-07-09 22:02:49 +0200

    nothing

--- neovim/.config/nvim/lua/extensions/cmp.lua
+++ neovim/.config/nvim/lua/extensions/cmp.lua
@@ -4,13 +4,13 @@
   See: https://github.com/hrsh7th/nvim-cmp
 ]]
 
-local cmp = require('cmp')
-local lspkind = require('lspkind')
+local cmp = require("cmp")
+local lspkind = require("lspkind")
 
-cmp.setup {
+cmp.setup({
   snippet = {
     expand = function(args)
-      require 'luasnip'.lsp_expand(args.body) -- Luasnip expand
+      require("luasnip").lsp_expand(args.body) -- Luasnip expand
     end,
   },
 
@@ -18,38 +18,37 @@ cmp.setup {
   mapping = {
 
     -- Autocompletion menu
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i' }),
-    ['<CR>'] = cmp.config.disable,                      -- Turn off autocomplete on <CR>
-    ['<C-i>'] = cmp.mapping.confirm({ select = true }), -- Turn on autocomplete on <C-i>
+    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i" }),
+    ["<CR>"] = cmp.config.disable, -- Turn off autocomplete on <CR>
+    ["<C-i>"] = cmp.mapping.confirm(), -- Turn on autocomplete on <C-i>
 
     -- Use <C-e> to abort autocomplete
-    ['<C-e>'] = cmp.mapping({
+    ["<C-e>"] = cmp.mapping({
       i = cmp.mapping.abort(), -- Abort completion
       c = cmp.mapping.close(), -- Close completion window
     }),
 
     -- Use <C-p> and <C-n> to navigate through completion variants
-    ['<C-k>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
-    ['<C-j>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+    ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+    ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
   },
 
   sources = cmp.config.sources({
-    { name = 'nvim_lsp' },                -- LSP
-    { name = 'nvim_lsp_signature_help' }, -- LSP for parameters in functions
-    { name = 'nvim_lua' },                -- Lua Neovim API
-    { name = 'luasnip' },                 -- Luasnip
-    { name = 'buffer' },                  -- Buffers
-    { name = 'path' },                    -- Paths
-    { name = "emoji" },                   -- Emoji
-  }, {
-  }),
+    { name = "nvim_lsp" }, -- LSP
+    { name = "nvim_lsp_signature_help" }, -- LSP for parameters in functions
+    { name = "nvim_lua" }, -- Lua Neovim API
+    { name = "luasnip" }, -- Luasnip
+    { name = "buffer" }, -- Buffers
+    { name = "path" }, -- Paths
+    { name = "emoji" }, -- Emoji
+  }, {}),
   formatting = {
     format = lspkind.cmp_format({
-      mode = 'symbol', -- Show only symbol annotations
-      maxwidth = 50,   -- Prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-    })
-  }
-}
+      mode = "symbol", -- Show only symbol annotations
+      maxwidth = 50, -- Prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+    }),
+  },
+})
 
 -- Add snippets from Friendly Snippets
 require("luasnip/loaders/from_vscode").lazy_load()
