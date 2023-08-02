f7cb4fb05a92d8b37e4dbbfa64426b0fbc83f817
Timothy Pescatore <timothy.pescatore@digitecgalaxus.ch>
2023-08-02 15:43:50 +0200

    formatting

--- neovim/.config/nvim/lua/config/autocmds.lua
+++ neovim/.config/nvim/lua/config/autocmds.lua
@@ -12,17 +12,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 vim.api.nvim_create_autocmd("FocusGained", { command = "checktime" })
 
 -- Go to last loc when opening a buffer
-vim.api.nvim_create_autocmd("BufReadPre", {
-  pattern = "*",
-  callback = function()
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "<buffer>",
-      once = true,
-      callback = function()
-        vim.cmd(
-          [[if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif]]
-        )
-      end,
-    })
-  end,
-})
+--vim.api.nvim_create_autocmd("BufReadPre", {
+--  pattern = "*",
+--  callback = function()
+--    vim.api.nvim_create_autocmd("FileType", {
+--      pattern = "<buffer>",
+--      once = true,
+--      callback = function()
+--        vim.cmd(
+--          [[if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif]]
+--        )
+--      end,
+--    })
+--  end,
+--})
