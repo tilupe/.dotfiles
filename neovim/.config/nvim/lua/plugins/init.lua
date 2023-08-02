2d4678b54e67df43203752e31a173fc550720bd9
Timothy Pescatore <timothy.pescatore@digitecgalaxus.ch>
2023-08-02 15:44:23 +0200

    add more mini plugins

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -113,15 +113,77 @@ return {
 
   -- Markdown
   { "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
-  { "iamcco/markdown-preview.nvim", config = true, cmd = "MarkdownPreview" },
 
   ---- Mini -----
+  --{
+  --  "echasnovski/mini.animate",
+  --  event = "VeryLazy",
+  --  version = false,
+  --  config = function(_, opts)
+  --    require("mini.animate").setup(opts)
+  --  end,
+  --},
+  {
+    "echasnovski/mini.clue",
+    event = "VeryLazy",
+    version = false,
+    config = function()
+      local miniclue = require("mini.clue")
+      miniclue.setup({
+        triggers = {
+          -- Leader triggers
+          { mode = "n", keys = "<Leader>" },
+          { mode = "x", keys = "<Leader>" },
+
+          -- Built-in completion
+          { mode = "i", keys = "<C-x>" },
+
+          -- `g` key
+          { mode = "n", keys = "g" },
+          { mode = "x", keys = "g" },
+
+          -- Marks
+          { mode = "n", keys = "'" },
+          { mode = "n", keys = "`" },
+          { mode = "x", keys = "'" },
+          { mode = "x", keys = "`" },
+
+          -- Registers
+          { mode = "n", keys = '"' },
+          { mode = "x", keys = '"' },
+          { mode = "i", keys = "<C-r>" },
+          { mode = "c", keys = "<C-r>" },
+
+          -- Window commands
+          { mode = "n", keys = "<C-w>" },
+
+          -- `z` key
+          { mode = "n", keys = "z" },
+          { mode = "x", keys = "z" },
+        },
+
+        clues = {
+          -- Enhance this by adding descriptions for <Leader> mapping groups
+          miniclue.gen_clues.builtin_completion(),
+          miniclue.gen_clues.g(),
+          miniclue.gen_clues.marks(),
+          miniclue.gen_clues.registers(),
+          miniclue.gen_clues.windows(),
+          miniclue.gen_clues.z(),
+        },
+      })
+    end,
+  },
   {
     "echasnovski/mini.files",
     event = "VeryLazy",
     config = function(_, opts)
       require("mini.files").setup(opts)
     end,
+    keys = {
+      { "<leader>ff", "<cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<cr>", desc = "Mini Files" },
+      { "<leader>fF", "<cmd>lua MiniFiles.open()<cr>", desc = "Mini Files" },
+    },
   },
   -- auto pairs
   {
@@ -134,43 +196,54 @@ return {
   -- surround
   {
     "echasnovski/mini.surround",
-    keys = function(_, keys)
-      -- Populate the keys based on the user's options
-      local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
-      local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-      local mappings = {
-        { opts.mappings.add, desc = "Add surrounding", mode = { "n", "v" } },
-        { opts.mappings.delete, desc = "Delete surrounding" },
-        { opts.mappings.find, desc = "Find right surrounding" },
-        { opts.mappings.find_left, desc = "Find left surrounding" },
-        { opts.mappings.highlight, desc = "Highlight surrounding" },
-        { opts.mappings.replace, desc = "Replace surrounding" },
-        { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
-      }
-      mappings = vim.tbl_filter(function(m)
-        return m[1] and #m[1] > 0
-      end, mappings)
-      return vim.list_extend(mappings, keys)
+    --keys = function(_, keys)
+    --  -- Populate the keys based on the user's options
+    --  local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
+    --  local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+    --  local mappings = {
+    --    { opts.mappings.add, desc = "Add surrounding", mode = { "n", "v" } },
+    --    { opts.mappings.delete, desc = "Delete surrounding" },
+    --    { opts.mappings.find, desc = "Find right surrounding" },
+    --    { opts.mappings.find_left, desc = "Find left surrounding" },
+    --    { opts.mappings.highlight, desc = "Highlight surrounding" },
+    --    { opts.mappings.replace, desc = "Replace surrounding" },
+    --    { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
+    --  }
+    --  mappings = vim.tbl_filter(function(m)
+    --    return m[1] and #m[1] > 0
+    --  end, mappings)
+    --  return vim.list_extend(mappings, keys)
+    --end,
+    --opts = {
+    --  mappings = {
+    --    add = "gsa", -- Add surrounding in Normal and Visual modes
+    --    delete = "gsd", -- Delete surrounding
+    --    find = "gsf", -- Find surrounding (to the right)
+    --    find_left = "gsF", -- Find surrounding (to the left)
+    --    highlight = "gsh", -- Highlight surrounding
+    --    replace = "gsr", -- Replace surrounding
+    --    update_n_lines = "gsn", -- Update `n_lines`
+    --  },
+    --},
+    --config = function(_, opts)
+    --  -- use gz mappings instead of s to prevent conflict with leap
+    --  require("mini.surround").setup(opts)
+    --end,
+    event = "VeryLazy",
+    config = function()
+      require("mini.surround").setup({
+        -- Module mappings. Use `''` (empty string) to disable one.
+        mappings = {
+          add = "gsa", -- Add surrounding in Normal and Visual modes
+          delete = "gsd", -- Delete surrounding
+          find = "gsf", -- Find surrounding (to the right)
+          find_left = "gsF", -- Find surrounding (to the left)
+          highlight = "gsh", -- Highlight surrounding
+          replace = "gsr", -- Replace surrounding
+          update_n_lines = "gsn", -- Update `n_lines`
+        },
+      })
     end,
-    opts = {
-      mappings = {
-        add = "gsa", -- Add surrounding in Normal and Visual modes
-        delete = "gsd", -- Delete surrounding
-        find = "gsf", -- Find surrounding (to the right)
-        find_left = "gsF", -- Find surrounding (to the left)
-        highlight = "gsh", -- Highlight surrounding
-        replace = "gsr", -- Replace surrounding
-        update_n_lines = "gsn", -- Update `n_lines`
-      },
-    },
-    config = function(_, opts)
-      -- use gz mappings instead of s to prevent conflict with leap
-      require("mini.surround").setup(opts)
-    end,
-    keys = {
-      { "<leader>ff", "<cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<cr>", desc = "Mini Files" },
-      { "<leader>fF", "<cmd>lua MiniFiles.open()<cr>", desc = "Mini Files" },
-    },
   },
 
   -- buffer remove
