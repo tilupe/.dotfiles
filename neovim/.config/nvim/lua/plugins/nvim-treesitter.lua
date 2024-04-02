6b6980832482bbd41f1d44591542ad5a2aa698d2
Timothy Pescatore <timothy.pescatore@digitecgalaxus.ch>
2024-04-02 22:03:47 +0200

    enable all treesitte modules

--- neovim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ neovim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -17,4 +17,10 @@ require("nvim-treesitter.configs").setup({
   highlight = {
     enable = true,
   },
+  incremental_selection = {
+    enable = true,
+  },
+  indent = {
+    enable = true,
+  }
 })
