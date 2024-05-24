c35362ae556580cc4e5b54bbdcb7ac42f1d6c294
Timothy Pescatore <timothy.pescatore@digitecgalaxus.ch>
2024-05-24 16:24:45 +0200

    remove used colorschemes

--- neovim/.config/nvim/lua/plugins/monet.lua
+++
@@ -1,8 +0,0 @@
-require("monet").setup {
-    transparent_background = true,
-    semantic_tokens = true,
-    dark_mode = true,
-    highlight_overrides= {},
-    color_overrides = {},
-    styles = {},
-}
