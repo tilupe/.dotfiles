7d86e79070f2941bd9346b1aecf1e12cd265550f
Timothy Pescatore <timothy.pescatore@digitecgalaxus.ch>
2024-06-10 10:29:34 +0200

    add github copilot

--- neovim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ neovim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -2,6 +2,12 @@ local cmp = require 'cmp'
 local icons = require 'icons'
 local luasnip = require 'luasnip'
 
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+end
+
 cmp.setup {
   snippet = {
     expand = function(args)
@@ -19,8 +25,8 @@ cmp.setup {
       select = true,
     },
     ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
+      if cmp.visible() and has_words_before() then
+        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
       elseif luasnip.expand_or_locally_jumpable() then
         luasnip.expand_or_jump()
       else
@@ -38,15 +44,20 @@ cmp.setup {
     end, { 'i', 's' }),
   },
   sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-    { name = 'neorg' },
+    -- Copilot Source
+    { name = "copilot", group_index = 2 },
+    -- Other Sources
+    { name = "nvim_lsp", group_index = 2 },
+    { name = "path", group_index = 2 },
+    { name = "luasnip", group_index = 2 },
+    { name = 'neorg', group_index = 2 },
   },
   formatting = {
     fields = { 'kind', 'abbr', 'menu' },
     format = function(entry, item)
       local max_width = 0
       local source_names = {
+        copilot = "",
         nvim_lsp = '(LSP)',
         path = '(Path)',
         luasnip = '(Snip)',
@@ -67,5 +78,23 @@ cmp.setup {
       item.dup = duplicates[entry.source.name] or duplicates_default
       return item
     end,
+  },
+  sorting = {
+    priority_weight = 2,
+    comparators = {
+      require("copilot_cmp.comparators").prioritize,
+
+      -- Below is the default comparitor list and order for nvim-cmp
+      cmp.config.compare.offset,
+      -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+      cmp.config.compare.recently_used,
+      cmp.config.compare.locality,
+      cmp.config.compare.kind,
+      cmp.config.compare.sort_text,
+      cmp.config.compare.length,
+      cmp.config.compare.order,
+    },
   },
 }
