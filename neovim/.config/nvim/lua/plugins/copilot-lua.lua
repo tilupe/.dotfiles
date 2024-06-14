78082d982db40e62ab51711cbe7c0402b54cf71f
Timothy Pescatore <timothy.pescatore@digitecgalaxus.ch>
2024-06-14 14:22:42 +0200

    add copilot

--- neovim/.config/nvim/lua/plugins/copilot-lua.lua
+++ neovim/.config/nvim/lua/plugins/copilot-lua.lua
@@ -1,4 +1,43 @@
- require("copilot").setup({
-  suggestion = { enabled = false },
-  panel = { enabled = false },
+require('copilot').setup({
+  panel = {
+    enabled = false,
+    auto_refresh = false,
+    keymap = {
+      jump_prev = "[[",
+      jump_next = "]]",
+      accept = "<C-CR>",
+      refresh = "gr",
+      open = "<M-CR>"
+    },
+    layout = {
+      position = "bottom", -- | top | left | right
+      ratio = 0.4
+    },
+  },
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    debounce = 75,
+    keymap = {
+      accept = "<C-k>",
+      accept_word = false,
+      accept_line = false,
+      next = "<C-,>",
+      prev = "<C-.>",
+      dismiss = "<C-x>",
+    },
+  },
+  filetypes = {
+    yaml = false,
+    markdown = false,
+    help = false,
+    gitcommit = false,
+    gitrebase = false,
+    hgcommit = false,
+    svn = false,
+    cvs = false,
+    ["."] = false,
+  },
+  copilot_node_command = 'node', -- Node.js version must be > 18.x
+  server_opts_overrides = {},
 })
